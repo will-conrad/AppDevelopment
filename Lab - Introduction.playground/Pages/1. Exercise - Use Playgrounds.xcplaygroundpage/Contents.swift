0a46b18148eca87906005921b67462a0f82305c8
Will Conrad (Mac) <jwconrad23@gmail.com>
2022-09-09 12:58:58 -0400

    x

--- Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -3,14 +3,14 @@
 
  The code below prints a few short statements about what you have learned in this lesson. Open the console area and view the code's output.
  */
-print("I have learned the following:")iufoniufnirghnocqiugnoiuniug
+print("I have learned the following:")
 print("What features make Swift a modern and safe language")
 print("How to use the Swift REPL in Terminal")
 print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("Hello, world!")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
