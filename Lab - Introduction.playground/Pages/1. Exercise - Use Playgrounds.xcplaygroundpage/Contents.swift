7463197474b02d417aa47385e1f1b1ac9d8f41d9
Will Conrad (Mac) <jwconrad23@gmail.com>
2022-09-10 17:13:09 -0400

    hw done

--- Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -11,6 +11,10 @@ print("How to use playgrounds to make writing Swift fun and simple")
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
 print("Hello, world!")
+var num1 = 5
+var num2 = 6
+
+print("num1 is \(num1)")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
